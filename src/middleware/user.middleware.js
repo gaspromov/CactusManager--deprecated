893ee51e4f005d2a9b9c06b1adeb5701a06bc1d7
khotsevich <xozewitc@yandex.ru>
2020-10-31 19:11:22 +0300

    Закончил работу с лицензиями

---
+++ src/middleware/user.middleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+const { ACCESS_TOKEN_SECRET } = require('../config/keys')
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
+    if (!token) {
+      return res.status(401).json({ message: 'Нет авторизации' })
+    }
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET)
+    req.user = decoded
+    return next()
+  } catch (e) {
+    return res.status(401).json({ message: 'Нет авторизации' })
+  }
+}
