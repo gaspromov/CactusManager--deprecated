b0eb0d9acf2e61db12e5f7563fec359a80dc0937
khotsevich <xozewitc@yandex.ru>
2020-11-02 14:21:53 +0300

    Исправил ошибку с ключами

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,9 +1,16 @@
 const production = require('./production')
 const development = require('./development')
 
-module.exports = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return production
-  }
-  return development
+let config = {}
+
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  config = { ...production }
+} else {
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  config = { ...development }
+}
+module.exports = {
+  // eslint-disable-next-line node/no-unsupported-features/es-syntax
+  ...config
 }
