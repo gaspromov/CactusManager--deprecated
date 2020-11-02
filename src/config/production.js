6968aee2f29bbbc2b7d8caef46c1780758a7f692
khotsevich <xozewitc@yandex.ru>
2020-11-02 14:21:53 +0300

    Исправил ошибку с ключами

--- src/config/production.js
+++ src/config/production.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: 5000,
-  MONGODB_URI: 'mongodb://khotsevich:9iVvnj1ckemUFfiy@localhost:27017/manager',
+  MONGODB_URI:
+    'mongodb://khotsevich:9iVvnj1ckemUFfiy@localhost:27017/manager/?authSource=admin',
   ACCESS_TOKEN_SECRET:
     '55d9fb08b144f1eaf48d847954d3d75d1ffb6edb8abcaf534df47f9e90e6048c3f0cb65e1d6df069ac637dcfcc732c01f0c14607a66a36d2c931ab075280b753',
   BCRYPT_SALT: 12,
